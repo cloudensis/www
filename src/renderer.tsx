bf431345c3e76ab70df7e3462ca00a08cb4e42f9
kanaru-ssk <kanaru.ssk@gmail.com>
2026-09-14 21:01:24 +0900

    add tailwind

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -1,5 +1,5 @@
 import { jsxRenderer } from "hono/jsx-renderer";
-import { ViteClient } from "vite-ssr-components/hono";
+import { Link, ViteClient } from "vite-ssr-components/hono";
 
 export const renderer = jsxRenderer(({ children }) => {
 	return (
@@ -7,6 +7,7 @@ export const renderer = jsxRenderer(({ children }) => {
 			<head>
 				<link rel="icon" type="image/x-icon" href="/favicon.svg" />
 				<ViteClient />
+				<Link href="/src/style.css" rel="stylesheet" />
 			</head>
 			<body>{children}</body>
 		</html>
