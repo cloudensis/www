1cf039abf3a03d3ab3fd32b3d819c7bc1a03503f
kanaru-ssk <kanaru.ssk@gmail.com>
2026-09-14 21:02:27 +0900

    add meta tag

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -5,7 +5,11 @@ export const renderer = jsxRenderer(({ children }) => {
 	return (
 		<html lang="ja">
 			<head>
+				<meta charset="UTF-8" />
+				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+				<title>cloudensis</title>
 				<link rel="icon" type="image/x-icon" href="/favicon.svg" />
+
 				<ViteClient />
 				<Link href="/src/style.css" rel="stylesheet" />
 			</head>
