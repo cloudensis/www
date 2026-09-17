958b1dd33850b89c0dc67c566d851b78cbaf6a5e
kanaru-ssk <kanaru.ssk@gmail.com>
2026-09-17 19:12:01 +0900

    add privacy route

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,13 @@
 import { Hono } from "hono";
 import { renderer } from "#/src/interfaces/middleware/renderer";
 import { homeRoutes } from "#/src/interfaces/routes/index";
+import { privacyRoutes } from "#/src/interfaces/routes/privacy/index";
 
 const app = new Hono<{ Bindings: CloudflareBindings }>();
 
 app.use(renderer);
 
 app.route("/", homeRoutes);
+app.route("/", privacyRoutes);
 
 export default app;
